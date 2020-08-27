e9d7c5c2c4b3c0a3894a61afa0c3c45e506a1d52
maneedhar <maneedharsharma@gmail.com>
2020-08-28 01:08:08 +0530

    Slider Added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import seedColors from './seedColors';
 import Palette from './Palette';
+import { generatePalette } from './ColorHelpers';
 
 export default class App extends Component {
 	render() {
 		return (
 			<div>
-				<Palette {...seedColors[4]} />
+				<Palette palette={generatePalette(seedColors[4])} />
 			</div>
 		);
 	}
